767497551711b51703fade39b61171a77ecb0502
Oskari Jokinen <oskari.jokinen@helsinki.fi>
2015-01-20 14:40:33 +0200

    Name for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120112700) do
+ActiveRecord::Schema.define(version: 20150120123523) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150120112700) do
     t.text     "about"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "name"
   end
 
 end
