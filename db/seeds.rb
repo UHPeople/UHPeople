43a4ac98c30ede5a0859085da59f64f6aea83573
Oskari Jokinen <oskari.jokinen@helsinki.fi>
2015-01-20 15:14:32 +0200

    The real seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-user = User.create([{ username: 'asd' }])
-user.hashtags.create(tag: 'asd')
+user = User.create username: 'asd', email: 'asd@asd.fi', campus:'Viikki', unit:'Maametsis', profilePicture: 'http://www.mycatspace.com/wp-content/uploads/2013/08/adopting-a-cat.jpg'
+user.hashtags.create tag: 'asd'
+user.hashtags.create tag: 'lätkämutsit'
+user.hashtags.create tag: 'avantouinti'
+
 
