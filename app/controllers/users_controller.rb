9138781bb1a976409ec894fbdfbc55114cba77ee
makroma <markoma@iki.fi>
2015-01-20 15:50:16 +0200

    basic user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+     @user = User.find(params[:id])
   end
 
 end
