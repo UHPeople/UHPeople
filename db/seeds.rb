63315525fbff64ff713c49dbcbc8a5b4590ab819
Riku Walve <riku.walve@gmail.com>
2015-01-22 12:44:28 +0200

    profile shows name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-user = User.create username: 'asd', email: 'asd@asd.fi', campus:'Viikki', unit:'Maametsis', profilePicture: 'http://www.mycatspace.com/wp-content/uploads/2013/08/adopting-a-cat.jpg'
-user.hashtags.create tag: 'asd'
+user = User.create username: 'asd', email: 'asd@asd.fi', campus:'Viikki', unit:'Maametsis', 
+	profilePicture: 'http://www.mycatspace.com/wp-content/uploads/2013/08/adopting-a-cat.jpg',
+	name: 'Johanna Tukiainen', about: ''
+
 user.hashtags.create tag: 'lätkämutsit'
 user.hashtags.create tag: 'avantouinti'
 
