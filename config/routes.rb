438af5c64a948c9333dfa035413ab886ba6abd6c
Silphidae <krista.vaananen@helsinki.fi>
2015-01-27 11:48:03 +0200

    edit user form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'users/:id' => 'users#show'
-
+  resources :users, :only => [:show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
