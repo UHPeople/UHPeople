a2d2eb515d4f92d4bb69230f83f240e24fd562c2
Riku Walve <riku.walve@gmail.com>
2015-02-04 12:25:14 +0200

    test for send button

--- spec/features/hashtag_page_chat_spec.rb
+++ spec/features/hashtag_page_chat_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe "Hashtag page chat" do
   let(:hashtag) { FactoryGirl.create(:hashtag) }
 
+  it "has send button" do
+    visit "/hashtags/#{hashtag.id}"
+
+    expect(page).to have_content('Send')
+  end
+
   # Not working because of the websocket-thingy?
   # it "can be written a message" do
   #  visit "/hashtags/#{hashtag.id}"
