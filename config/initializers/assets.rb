4d956c34af83597f21a0cc80d89534bc11109e1e
Riku Walve <riku.walve@gmail.com>
2015-02-05 14:05:25 +0200

    switch bootstrap gems

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -10,10 +10,10 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += ["fontawesome-webfont.ttf",
-                             					"fontawesome-webfont.eot",
-					                            "fontawesome-webfont.svg",
-					                            "fontawesome-webfont.woff"]
+#Rails.application.config.assets.precompile += ["fontawesome-webfont.ttf",
+#                             					"fontawesome-webfont.eot",
+#					                            "fontawesome-webfont.svg",
+#					                            "fontawesome-webfont.woff"]
 
-Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings.png )
-Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings-white.png )
+#Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings.png )
+#Rails.application.config.assets.precompile += %w( twitter/bootstrap/glyphicons-halflings-white.png )
