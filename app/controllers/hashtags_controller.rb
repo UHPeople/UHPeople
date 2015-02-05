0e46db4f052935f3b3d1f6bd9658f2bfe957d725
makroma <markoma@iki.fi>
2015-02-05 10:59:57 +0200

    user is added as member to created hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,10 +1,16 @@
 class HashtagsController < ApplicationController
 	before_action :set_hashtag, only: [:show]
 
+  def show
+    
+    
+  end  
+
+
   def create
     @hashtag = Hashtag.new(tag: params[:tag])
-    
     if @hashtag.save
+      current_user.hashtags << @hashtag
       redirect_to @hashtag
     end
   end   
