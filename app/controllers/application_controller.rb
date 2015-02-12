9ae0302f8551c773cd41ce9c76486c0dbb361b4c
makroma <markoma@iki.fi>
2015-02-12 13:42:52 +0200

    feed message limit lenght and wrap width

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :split_str
 
   def current_user
     return nil if session[:user_id].nil?
     User.find(session[:user_id])
   end
+  
+  def split_str(txt, col = 40)
+	  txt.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/,
+	    "\\1\\3\n") 
+	end
 end
