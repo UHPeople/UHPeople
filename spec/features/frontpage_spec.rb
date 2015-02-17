78fc5e545088bbafc42ddfdfad2a3127a3285b56
Riku Walve <riku.walve@gmail.com>
2015-02-17 14:03:38 +0200

    fix broken tests

--- spec/features/frontpage_spec.rb
+++ spec/features/frontpage_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe "Frontpage page" do
-
   it "has login button" do
-
-    visit "/"
+    visit root_path
     expect(page).to have_content 'Login'
+  end
+
+  it "redirects to feed" do
+    user = FactoryGirl.create(:user)
+    visit "/login/#{user.id}"
+    
+    visit root_path
+    expect(page.current_path).to eq "/feed"
   end
 end
