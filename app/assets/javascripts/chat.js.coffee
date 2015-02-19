2c104a1b6dea4611c030ff1d984152f2fae2d443
Riku Walve <riku.walve@gmail.com>
2015-02-19 13:42:45 +0200

    refactor chat backend and start work on online status

--- app/assets/javascripts/chat.js.coffee
+++ app/assets/javascripts/chat.js.coffee
@@ -1,32 +1,54 @@
+set_online = (id) ->
+  $('li#' + id).append 'online'
+
+add_message = (data) ->
+  hashtag = $('#hashtag-id')[0].value
+
+  if data.hashtag != hashtag
+    return
+  
+  $('#chat-text').append '<div class=\'panel-body\'><span class=\'chat-timestamp\'>' +
+    data.timestamp + '</span> <span class=\'chat-name\'>' + data.user + '</span>: ' + data.content + '</div>'
+  
+  $('#chat-text').stop().animate {
+    scrollTop: $('#chat-text')[0].scrollHeight
+  }, 800
+
+exports = this
+exports.add_message = add_message
+exports.set_online = set_online
+
 ready = ->
   scheme = 'ws://'
   uri = scheme + window.document.location.host
   ws = new WebSocket(uri)
+
+  exports.ws = ws
 
   if not $('#hashtag-id').length
     return
   
   hashtag = $('#hashtag-id')[0].value
   user = $('#user-id')[0].value
-
-  # ws.send JSON.stringify(event: 'online', hashtag: hashtag, user: user)
   
   $('#chat-text').scrollTop $('#chat-text')[0].scrollHeight
 
+  ws.onopen = ->
+    console.log("Connection Opened")
+    #ws.send JSON.stringify(event: 'online', hashtag: hashtag, user: user)
+
   ws.onmessage = (message) ->
+    console.log(message)
+
     data = JSON.parse(message.data)
-    
-    if data.hashtag != hashtag
-      return
-    
-    $('#chat-text').append '<div class=\'panel-body\'><span class=\'chat-timestamp\'>' +
-      data.timestamp + '</span> <span class=\'chat-name\'>' + data.user + '</span>: ' + data.content + '</div>'
-    
-    $('#chat-text').stop().animate {
-      scrollTop: $('#chat-text')[0].scrollHeight
-    }, 800
-    
-    return
+
+    if data.event == 'message'
+      add_message(data)
+    else if data.event == 'online'
+      set_online id for user in data.onlines
+
+  ws.onclose = ->
+    console.log("Connection Closed")
 
   $('#input-form').on 'submit', (event) ->
     event.preventDefault()
@@ -40,8 +62,6 @@ ready = ->
       user: user)
 
     $('#input-text')[0].value = ''
-    return
-  return
 
 $(document).ready(ready)
 $(document).on('page:load', ready)
--- lib/chat_backend.rb
+++ lib/chat_backend.rb
@@ -5,6 +5,17 @@ require 'erb'
 
 module UHPeople
   class ChatBackend
+    class Client
+      def new(socket, user)
+        @socket = socket
+        @user = user
+      end
+
+      def send(data)
+        @socket.send(data)
+      end
+    end
+
     KEEPALIVE_TIME = 15
 
     def initialize(app)
@@ -13,44 +24,34 @@ module UHPeople
     end
 
     def call(env)
-      if Faye::WebSocket.websocket?(env)
+      if Faye::WebSocket.websocket? env
         ws = Faye::WebSocket.new(env, nil, {ping: KEEPALIVE_TIME })
 
         ws.on :open do |event|
-          @clients << ws
+          p "open"
+
+          @clients << ws #Client.new(ws, nil)
         end
 
         ws.on :message do |event|
+          p "message"
+
           data = JSON.parse(event.data)
-
-          if data['event'] == 'message'
-            user = User.find(data['user'])
-            hashtag = Hashtag.find(data['hashtag'])
-
-            if user.hashtags.include? hashtag
-              message = Message.create content: data['content'],
-                                       hashtag_id: data['hashtag'],
-                                       user_id: data['user']
-
-              data['user'] = user.name
-              data['timestamp'] = message.timestamp
-
-              if message.valid?
-                @clients.each { |client| client.send(sanitize(data)) }
-              end
-            end
-          elsif data['event'] == 'online'
-            # @clients.each { |client| client.send() }
-          end 
+          response = generate_response data
+          @clients.each { |client| client.send(response) }
         end
 
         ws.on :close do |event|
+          p "close"
+
           @clients.delete(ws)
           ws = nil
+
+          onlines = online_users
+          @clients.each { |client| client.send(onlines) }
         end
 
         ws.rack_response
-
       else
         @app.call(env)
       end
@@ -61,6 +62,41 @@ module UHPeople
     def sanitize(json)
       json.each {|key, value| json[key] = ERB::Util.html_escape(value) }
       JSON.generate(json)
+    end
+
+    def online_users
+      onlines = @clients.map { |client| client.user.id }
+      json = {'event': 'online', 'onlines': onlines}
+      
+      p json
+
+      return JSON.generate(json)
+    end
+
+    def generate_response(data)
+      if data['event'] == 'message'
+        p "message event"
+
+        user = User.find(data['user'])
+        hashtag = Hashtag.find(data['hashtag'])
+
+        if user.hashtags.include? hashtag
+          message = Message.create content: data['content'],
+                                   hashtag_id: data['hashtag'],
+                                   user_id: data['user']
+
+          data['user'] = user.name
+          data['timestamp'] = message.timestamp
+
+          if message.valid?
+            return sanitize(data)
+          end
+        end
+      elsif data['event'] == 'online'
+        p "online event"
+
+        return online_users
+      end 
     end
   end
 end
