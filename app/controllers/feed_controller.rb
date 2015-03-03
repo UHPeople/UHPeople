dd803d2f540d44fb6d0fbac92c94ac6562ae135b
Riku Walve <riku.walve@gmail.com>
2015-03-03 14:17:58 +0200

    list all messages in feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,6 +2,11 @@ class FeedController < ApplicationController
   before_action :require_login
 
   def index
-    @chats = current_user.hashtags.map { |tag| tag.messages.last(5) }
+    tags = current_user.hashtags
+    @chats = tags.map { |tag| tag.messages.last(5) }
+
+    @messages = Message.includes(:hashtag).all.map { |message| message if tags.include? message.hashtag }
+    @messages.compact.sort_by { |message| message.timestamp }
+    @messages = @messages.reverse!
   end
 end 
