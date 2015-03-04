61deecff2e471fa4dc43ee596305e6bb9fe24bb5
Silphidae <krista.vaananen@helsinki.fi>
2015-03-04 13:34:51 +0200

    only favourites shown in feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -2,9 +2,10 @@ class FeedController < ApplicationController
   before_action :require_login
 
   def index
-    tags = current_user.hashtags
-    @chats = tags.map { |tag| tag.messages.last(5) }
+    fav_tags = current_user.user_hashtags.where(favourite:true)
+    @chats = fav_tags.map { |tag| tag.messages.last(5) }
 
+    tags = current_user.hashtags
     @messages = Message.includes(:hashtag).all.map { |message| message if tags.include? message.hashtag }
     @messages.compact.sort_by { |message| message.timestamp }
     @messages = @messages.reverse!
