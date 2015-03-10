dc2da6259fff34f560aade4ddd9a787dac447aa7
makroma <markoma@iki.fi>
2015-03-10 13:02:36 +0200

    frontpage conflict

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,13 +1,7 @@
 class FrontpageController < ApplicationController
   def index
-<<<<<<< HEAD
     @login_url = ENV['RAILS_ENV'] == "production" ? '/auth/shibboleth' : users_path
     
-    if not current_user.nil?
-      redirect_to feed_index_path
-    end
-=======
     redirect_to feed_index_path unless current_user.nil?
->>>>>>> dev
   end
 end
