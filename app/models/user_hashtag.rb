50cc565d197974e3734330eb82987c06a224d010
Silphidae <krista.vaananen@helsinki.fi>
2015-03-10 12:43:38 +0200

    model bug fixing

--- app/models/user_hashtag.rb
+++ app/models/user_hashtag.rb
@@ -1,5 +1,6 @@
 class UserHashtag < ActiveRecord::Base
-  validates :user_id, :hashtag_id, presence: true
+  validates :user_id, presence: true, uniqueness:{ scope: :hashtag_id }
+  validates :hashtag_id, presence: true
 
   belongs_to :user
   belongs_to :hashtag
