5fe7e5b8e6fbcb63e520853d53d687ec7c90c358
Riku Walve <riku.walve@gmail.com>
2015-03-12 10:59:57 +0200

    sort favourite chatboxes by latest message

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,6 +4,7 @@ class FeedController < ApplicationController
   def index
     fav_tags = current_user.user_hashtags.where(favourite: true)
     @chats = fav_tags.map { |tag| tag.hashtag.messages.last(5) }
+    @chats.compact.sort_by { |chat| chat.last.timestamp }
 
     tags = current_user.hashtags
     @messages = Message.includes(:hashtag).all.map { |message| message if tags.include? message.hashtag }
