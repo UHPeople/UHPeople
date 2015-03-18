2428bd0647f4e01ca46d6af8b6ff575b13aa9fdf
makroma <markoma@iki.fi>
2015-03-18 12:30:24 +0200

    notification model with trickers. invitation partial

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,5 @@
 class Notification < ActiveRecord::Base
 	belongs_to :user
+	has_one :tricker_user, class_name: 'User', foreign_key: 'id'
+	has_one :tricker_hashtag, class_name: 'Hashtag', foreign_key: 'id'
 end
