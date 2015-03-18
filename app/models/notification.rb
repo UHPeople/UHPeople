1ef0c301f0963cbe03208d3642e3dfdab2b7c2bd
makroma <markoma@iki.fi>
2015-03-18 12:52:17 +0200

    trickers belongs to plus some candy love

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ActiveRecord::Base
 	belongs_to :user
-	has_one :tricker_user, class_name: 'User', foreign_key: 'id'
-	has_one :tricker_hashtag, class_name: 'Hashtag', foreign_key: 'id'
+	belongs_to :tricker_user, class_name: 'User', foreign_key: 'tricker_user_id'
+	belongs_to :tricker_hashtag, class_name: 'Hashtag', foreign_key: 'tricker_hashtag_id'
 end
