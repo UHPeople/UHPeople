9eb597b44b791bdc6566b7b23e9eb388dfa08f64
makroma <markoma@iki.fi>
2015-03-18 09:58:49 +0200

    notification db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313173646) do
+ActiveRecord::Schema.define(version: 20150318075106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20150313173646) do
     t.integer  "hashtag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "type",                              null: false
+    t.integer "user_id",                           null: false
+    t.string  "tricker_user_id"
+    t.integer "tricker_hashtag_id"
+    t.boolean "visible",            default: true, null: false
   end
 
   create_table "user_hashtags", force: :cascade do |t|
