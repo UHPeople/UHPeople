017bfd233b00b36c9cefc2b02b552a9591407d67
Riku Walve <riku.walve@gmail.com>
2015-03-19 12:49:53 +0200

    add missing image

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,14 +3,14 @@ class FeedController < ApplicationController
 
   def index
     @user_tags = current_user.user_hashtags.includes(hashtag: :messages)
-      .order('user_hashtags.favourite desc', 'hashtags.updated_at desc')
+                 .order('user_hashtags.favourite desc', 'hashtags.updated_at desc')
 
     fav_user_tags = @user_tags.favourite.joins(hashtag: :messages).uniq
     @chats = fav_user_tags.map { |tag| tag.hashtag.messages.last(5) }
 
     tags = @user_tags.map(&:hashtag)
     @messages = Message.includes(:hashtag, :user)
-      .where(hashtag: tags)
-      .order(created_at: :desc).limit(20)
+                .where(hashtag: tags)
+                .order(created_at: :desc).limit(20)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   has_many :hashtags, through: :user_hashtags
   has_many :messages, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_file_name :avatar, :matches => [/png\Z/, /jpe?g\Z/]
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '45x45>' },
+                             default_url: 'missing.png'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 end
