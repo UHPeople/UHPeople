2a6c08db51ede2c51267b814adaf2e9ccaf8942a
Riku Walve <riku.walve@gmail.com>
2015-03-24 14:44:17 +0200

    slightly refactor tagcloud

--- lib/tagcloud_logic.rb
+++ lib/tagcloud_logic.rb
@@ -11,24 +11,20 @@ class TagcloudLogic
     most_usrs = sort_and_first(most_usrs)
 
     for i in 0..(newest_msgs.length - 1)
-      find_duplicates(i, most_new_users, newest_msgs)
-      find_duplicates(i, most_usrs, newest_msgs)
+      join_duplicates(newest_msgs[i], most_new_users)
+      join_duplicates(newest_msgs[i], most_usrs)
     end
 
     set_weights(newest_msgs)
     set_weights(most_new_users)
     set_weights(most_usrs)
 
-    newest_msgs.concat(most_new_users)
-    newest_msgs.concat(most_usrs)
-    newest_msgs.sort_by { |k| k[1] }
-
-    newest_msgs.first(20)
+    newest_msgs.concat(most_new_users).concat(most_usrs).first(20)
   end
 
-  def set_weights(tag)
-    for j in 0..(tag.length - 1)
-      tag[j][1] = 30 - j
+  def set_weights(array)
+    for j in 0..(array.length - 1)
+      array[j][1] = 30 - j
     end
   end
 
@@ -38,31 +34,33 @@ class TagcloudLogic
       new_messages = tag.messages.where('created_at >=?', 1.week.ago).count
       new_users = tag.user_hashtags.where('created_at >=?', 1.week.ago).count
       users = tag.users.count
+
       newest_msgs << [name, new_messages, tag.id]
       most_new_users << [name, new_users, tag.id]
       most_usrs << [name, users, tag.id]
     end
   end
 
-  def find_duplicates(i, most_new_users, newest_msgs)
-    for j in 0..(most_new_users.length - 1)
-      if newest_msgs[i][0].equal? most_new_users[j][0]
-        newest_msgs[i][1] = newest_msgs[i][1] + most_new_users[j][1]
-        most_new_users[j][1] = 0
+  def join_duplicates(asd, array)
+    for j in 0..(array.length - 1)
+      if asd[0].equal? array[j][0]
+        asd[1] = asd[1] + array[j][1]
+        array[j][1] = 0
       end
     end
   end
 
   def make_cloud(tag_array)
     word_array = []
+    
     tag_array.each do |tag|
       word_array.push(text: tag[0], weight: tag[1], link: "/hashtags/#{tag[2]}")
     end
+
     word_array
   end
 
   def sort_and_first(to_sort)
-    to_sort = to_sort.sort_by { |k| k[1] }.reverse!
-    to_sort.first(30)
+    to_sort.sort_by { |k| k[1] }.reverse!.first(30)
   end
 end
