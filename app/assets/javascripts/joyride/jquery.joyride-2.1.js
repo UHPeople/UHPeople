818eb1b6f7028a44a06878ae94ee073830ff3dc9
makroma <markoma@iki.fi>
2015-03-24 15:54:20 +0200

    joyride css, js, db

--- app/assets/javascripts/joyride/jquery.joyride-2.1.js
+++ app/assets/javascripts/joyride/jquery.joyride-2.1.js
@@ -40,11 +40,11 @@
       'preStepCallback'      : $.noop,    // A method to call before each step
       'postStepCallback'     : $.noop,    // A method to call after each step
       'template' : { // HTML segments for tip layout
-        'link'    : '<a href="#close" class="joyride-close-tip">X</a>',
+        'link'    : '<a href="#close" class="joyride-close-tip btn btn-danger pull-right">end tour</a>',
         'timer'   : '<div class="joyride-timer-indicator-wrap"><span class="joyride-timer-indicator"></span></div>',
         'tip'     : '<div class="joyride-tip-guide"><span class="joyride-nub"></span></div>',
         'wrapper' : '<div class="joyride-content-wrapper" role="dialog"></div>',
-        'button'  : '<a href="#" class="joyride-next-tip"></a>',
+        'button'  : '<a href="#" class="joyride-next-tip btn btn-primary"></a>',
         'modal'   : '<div class="joyride-modal-bg"></div>',
         'expose'  : '<div class="joyride-expose-wrapper"></div>',
         'exposeCover': '<div class="joyride-expose-cover"></div>'
@@ -129,6 +129,9 @@
 
             settings.$document.on('click.joyride', '.joyride-close-tip', function (e) {
               e.preventDefault();
+              /*This is gointo be place for ajax post current_user.first_time = false */
+
+
               methods.end(true /* isAborted */);
             });
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319072045) do
+ActiveRecord::Schema.define(version: 20150324132317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,14 +56,15 @@ ActiveRecord::Schema.define(version: 20150319072045) do
     t.string   "campus"
     t.string   "unit"
     t.text     "about"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "name"
     t.string   "title"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "first_time",          default: true
   end
 
 end
