c45f508df676fe529b156383e8a5743186efff2f
makroma <markoma@iki.fi>
2015-03-26 12:21:57 +0200

    type 2 notification from new topic

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -35,6 +35,14 @@ class HashtagsController < ApplicationController
   end
 
   def update
+    @hashtag.users.each do |user|
+      Notification.create notification_type: 2,
+                        user_id: user.id,
+                        tricker_user_id: @hashtag.topic_updater_id,
+                        tricker_hashtag_id: @hashtag.id
+      request.env['chat.notification_callback'].call(user.id)
+    end  
+
     respond_to do |format|
       if @hashtag.update(hashtag_params)
         format.html { redirect_to :back, notice: 'Topic was successfully updated.' }
