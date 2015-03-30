e5147507066237fece565385de4d2f989b4dd5cf
Riku Walve <riku.walve@gmail.com>
2015-03-30 12:43:20 +0300

    cleanupz

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -10,5 +10,5 @@ class Hashtag < ActiveRecord::Base
                                   default_url: 'missing.png'
   validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\Z/
   validates_attachment_file_name :cover_photo, matches: [/png\Z/, /jpe?g\Z/]
-  validates_attachment_size :cover_photo, :in => 0..10.megabytes
+  validates_attachment_size :cover_photo, in: 0..10.megabytes
 end
