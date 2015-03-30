64ffa2e5c51335c423607ca36a6cb62529add467
Riku Walve <riku.walve@gmail.com>
2015-03-30 13:29:09 +0300

    show favourites with no messages

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -7,8 +7,8 @@ class FeedController < ApplicationController
     @user_tags = current_user.user_hashtags.includes(hashtag: :messages)
                  .order('user_hashtags.favourite desc', 'hashtags.updated_at desc')
 
-    fav_user_tags = @user_tags.favourite.joins(hashtag: :messages).uniq
-    @chats = fav_user_tags.map { |tag| tag.hashtag.messages.last(5) }
+    fav_user_tags = @user_tags.favourite.uniq
+    @chats = fav_user_tags.map { |tag| { tag: tag.hashtag, messages: tag.hashtag.messages.last(5) } }
 
     tags = @user_tags.map(&:hashtag)
     @messages = Message.includes(:hashtag, :user)
