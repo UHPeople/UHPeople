ea66e9aa1b22e64687b0bd217c2cc52ab4daae6e
Silphidae <krista.vaananen@helsinki.fi>
2015-03-30 13:05:18 +0300

    invitation email to cs.helsinki.fi allowed

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -6,14 +6,14 @@ class InviteController < ApplicationController
     @user = current_user
     @receiver = params[:receiver]
     # tarkista että receiver loppuu @helsinki.fi
-    if @receiver.include? '@helsinki.fi'
+    if @receiver.include? ('@helsinki.fi'|| '@cs.helsinki.fi')
       if InvitationMailer.invitation_email(@user, @receiver).deliver_now
         redirect_to :root, notice: 'Invitation was sent succesfully.'
       else
         redirect_to :back, notice: 'Error sending invitation.'
       end
     else
-      redirect_to :back, notice: 'Recipient must be a "@helsinki.fi" email address.'
+      redirect_to :back, notice: 'Recipient must be a "helsinki.fi" email address.'
     end
   end
 end
