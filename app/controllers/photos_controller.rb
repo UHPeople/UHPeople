fd4615a79c466dc3e4a149ef804b9b99d9783c8f
Silphidae <krista.vaananen@helsinki.fi>
2015-03-31 13:32:21 +0300

    image renamed and photo upload working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,27 +3,20 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: [:show]
 
   def show
-
   end
 
   def create
-    @photo = Photo.create(photo_params)
-    if @photo.save
-      respond_to do |format|
+    @photo = Photo.create(user_id: current_user.id, image_text: params[:image_text], image: params[:image])
+    respond_to do |format|
+      if @photo.save
         format.html { redirect_to current_user, notice: 'Photo was successfully added.' }
-      end
-    else
-      respond_to do |format|
+      else
         format.html { redirect_to current_user, notice: 'An error occured while saving photo.' }
       end
     end
   end
 
   private
-
-  def photo_params
-    params.require(:photo).permit(:user_id, :photo_text, :photo)
-  end
 
   def set_photo
     @photo = Photo.find(params[:id])
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,9 +3,9 @@ class Photo < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :photo, styles: { medium: '300x300>', thumb: '45x45#' },
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '45x45#' },
                     default_url: 'missing.png'
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
-  validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/]
-  validates_attachment_size :photo, in: 0..10.megabytes
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
+  validates_attachment_size :image, in: 0..10.megabytes
 end
