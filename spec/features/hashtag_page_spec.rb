37d2d128b933d780a84862376b176d6533309f8d
makroma <markoma@iki.fi>
2015-04-01 12:27:27 +0300

    fix tests

--- spec/features/hashtag_page_spec.rb
+++ spec/features/hashtag_page_spec.rb
@@ -36,17 +36,17 @@ RSpec.describe Hashtag do
 
     context 'invitation box' do
       it 'doesn\'t send invitation to member' do
+        find('button[data-target="#invite"]').click
         fill_in 'user', with: user.name
-        click_button 'Invite'
-
+        find('input[value="Invite"]').click
         expect(find('.notif-count')).to_not have_content '1'
       end
 
       it 'sends invitation to non-member user' do
         user2 = User.create name: 'asd', username: 'asdasd'
-
+        find('button[data-target="#invite"]').click
         fill_in 'user', with: user2.name
-        click_button 'Invite'
+        find('input[value="Invite"]').click
 
         visit "/login/#{user2.id}"
 
