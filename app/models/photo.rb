273ceaba6dedfe09c60d82042bc9f8cdf48f8bd0
Riku Walve <riku.walve@gmail.com>
2015-04-14 11:44:23 +0300

    limit uploaded picture dimensions and quality

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,8 +4,21 @@ class Photo < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: '300x300>', thumb: '45x45#', small: '65x65#' },
-                            default_url: 'missing.png'
+  has_attached_file :image,
+    styles: {
+      medium: '300x300>',
+      thumb: '45x45#',
+      small: '65x65#',
+      original: '640x480>'
+    },
+    convert_options: {
+      medium: "-quality 75 -strip",
+      thumb: "-quality 75 -strip",
+      small: "-quality 75 -strip",
+      original: "-quality 75 -strip"
+    },    
+    default_url: 'missing.png'
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/i
   validates_attachment_file_name :image, matches: [/png\Z/i, /jpe?g\Z/i]
   validates_attachment_size :image, in: 0..10.megabytes
