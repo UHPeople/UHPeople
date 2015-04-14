86aa6870f8b07db70b0bfe8bd3c424547fe07643
Riku Walve <riku.walve@gmail.com>
2015-04-14 20:02:23 +0300

    let new users add photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,12 +43,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to feed_index_path
-    else
-      redirect_to action: 'new'
+    redirect_to action: 'new' && return unless @user.save
+
+    if params[:image]
+      photo = Photo.new(user_id: @user.id,image: params[:image], image_text: params[:image_text])
+      if photo.save
+        @user.update_attribute(:profilePicture, photo.id)
+      end
     end
+
+    session[:user_id] = @user.id
+    redirect_to feed_index_path
   end
 
   def set_first_time_use
@@ -107,7 +112,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :name, :title, :email, :campus, :unit, :about, :profilePicture)
+    params.require(:user).permit(:username, :name, :title, :email, :campus, :unit, :about, :image, :image_title)
   end
 
   def edit_user_params
