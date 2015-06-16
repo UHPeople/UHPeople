4954ea08f9908e8b40628ada1f16b2475a4867b9
makroma <markoma@iki.fi>
2015-06-16 16:57:55 +0300

    model method for unread message count + debug view in front page

--- app/models/user_hashtag.rb
+++ app/models/user_hashtag.rb
@@ -6,4 +6,12 @@ class UserHashtag < ActiveRecord::Base
   belongs_to :hashtag
 
   scope :favourite, -> { where favourite: true }
+  
+
+  def unread_messages 
+    #u.user_hashtags.last.hashtag.messages.where("created_at > ?", u.user_hashtags.last.last_visited ).count
+    self.hashtag.messages.where("created_at > ?", self.last_visited).count  if self.last_visited
+  end
 end
+
+
