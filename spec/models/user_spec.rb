06112185026434d2de702026661d302f8377116a
makroma <markoma@iki.fi>
2015-06-23 17:17:16 +0300

    fex most_active channel functio tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,31 @@ RSpec.describe User do
     expect(user.valid?).to be(false)
     expect(described_class.count).to eq(0)
   end
+
+  context 'has active and unactive channels' do
+    let!(:user) { FactoryGirl.create(:user) }
+    let!(:hashtag) { FactoryGirl.create(:hashtag) }
+    let!(:user_hashtag) { UserHashtag.create(user: user, hashtag: hashtag) }
+    let!(:message) {Message.create(content: 'aasd', user: user, hashtag: hashtag)}
+
+    it 'has user_hashtag is in most_active_channels' do
+      expect(user.six_most_active_channels.count).to be(1)
+    end
+
+    it 'has unactive channels empty' do
+      expect(user.unactive_channels.count).to be(0)
+    end
+    it 'has unactive channel with one' do
+      hashtag2 = Hashtag.create tag: 'avantouinti2'
+      UserHashtag.create(user: user, hashtag: hashtag2)
+      expect(user.unactive_channels.count).to be(1)
+    end
+    it 'has hashtag moved to active channels' do
+      hashtag2 = Hashtag.create tag: 'avantouinti2'
+      UserHashtag.create(user: user, hashtag: hashtag2)
+      Message.create(content: 'aasd', user: user, hashtag: hashtag2)
+      expect(user.unactive_channels.count).to be(0)
+      expect(user.six_most_active_channels.count).to be(2)
+    end
+  end
 end
