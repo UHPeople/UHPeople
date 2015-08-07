29170773ac1a560417d11c513f337b1aa7d94cc9
Riku Walve <riku.walve@gmail.com>
2015-08-07 12:23:51 +0300

    max favourites configurable

--- app/controllers/user_hashtags_controller.rb
+++ app/controllers/user_hashtags_controller.rb
@@ -2,17 +2,16 @@ class UserHashtagsController < ApplicationController
   before_action :require_login
 
   def update
-    @user_hashtag = UserHashtag.find(params[:id])
-    @max_faves = 5
+    user_hashtag = UserHashtag.find(params[:id])
 
-    if @user_hashtag.favourite?
-      @user_hashtag.update(favourite: false)
+    if user_hashtag.favourite?
+      user_hashtag.update(favourite: false)
     else
-      if UserHashtag.where(user_id: current_user.id, favourite: true).count < @max_faves
-        @user_hashtag.update(favourite: true)
+      if UserHashtag.where(user_id: current_user.id, favourite: true).count < APP_CONFIG['max_faves']
+        user_hashtag.update(favourite: true)
       else
         redirect_to feed_index_path(tab: 'favourites'),
-                    alert: "You already have #{@max_faves} favourites, remove some to add a new one!"
+                    alert: "You already have #{APP_CONFIG['max_faves']} favourites, remove some to add a new one!"
         return
       end
     end
