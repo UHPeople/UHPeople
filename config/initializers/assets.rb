9787f83cbf95671fdf8b8b9957fcd2b7d64929b9
Riku Walve <riku.walve@gmail.com>
2015-08-24 13:47:09 +0300

    fix heroku2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,8 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.2'
 
-Rails.application.config.assets.precompile += []
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "bootstrap", "fonts")
+Rails.application.config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.woff2 )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
