f48332841c19d034ef3d6902654800c6c49c1861
Silphidae <krista.vaananen@helsinki.fi>
2015-09-18 09:28:50 +0300

    notifications added but not working yet

--- lib/chat_backend.rb
+++ lib/chat_backend.rb
@@ -74,9 +74,11 @@ module UHPeople
       if like.nil?
         like = Like.new(user_id: user.id, message: message)
         return unless like.save
+        add_likenotif(message)
         like_callback('like', message)
       else
         like.destroy
+        remove_likenotif(message)
         like_callback('dislike', message)
       end
     end
@@ -188,6 +190,42 @@ module UHPeople
 
       notification_callback(user)
     end
+
+    def add_likenotif(message)
+      if (message.likes <= 1)
+        send_likenotif(message.user_id,
+                     current_user,
+                     message.hashtag_id,
+                     message)
+      else
+        update_likenotif(message.user_id, current_user)
+      end
+    end
+
+    def send_likenotif(user, tricker, hashtag, message)
+      Notification.create notification_type: 4,
+                          user_id: user,
+                          tricker_user_id: tricker,
+                          tricker_hashtag_id: hashtag,
+                          message: message
+
+      notification_callback(user)
+    end
+
+    def update_likenotif(user, tricker_user)
+      Notification.update tricker_user_id: tricker_user
+
+      notification_callback(user)
+    end
+
+    def remove_likenotif(message)
+      if (message.likes <= 1)
+        Notification.find(params[message_id: message, notification_type: 4, user_id: message.user_id]).destroy
+      else
+        update_likenotif(message.user_id, message.likes.last.user_id)
+      end
+    end
+
 
     def respond(socket, data)
       p data
