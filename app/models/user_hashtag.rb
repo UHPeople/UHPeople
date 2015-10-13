a7158c62c13973d40bf3bde2a82e7370946fcb2a
Riku Walve <riku.walve@gmail.com>
2015-10-13 13:59:59 +0300

    slightly optimize unread_messages

--- app/models/user_hashtag.rb
+++ app/models/user_hashtag.rb
@@ -6,9 +6,13 @@ class UserHashtag < ActiveRecord::Base
   belongs_to :hashtag
 
   scope :favourite, -> { where(favourite: true) }
-  scope :downcase_sorted, -> { includes(:hashtag).sort_by { |h| [h.favourite ? 0 : 1, h.hashtag.tag.downcase] } }
+  scope :downcase_sorted, -> {
+    includes(:hashtag).sort_by { |h| [h.favourite ? 0 : 1, h.hashtag.tag.downcase] }
+  }
 
   def unread_messages
-    hashtag.messages.where('created_at > ?', last_visited).count if last_visited
+    return hashtag.messages.count if last_visited.nil?
+    return 0 if last_visited < updated_at
+    hashtag.messages.where('created_at > ?', last_visited).count
   end
 end
