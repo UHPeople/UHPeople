4c180b6588e8b85f91ee894f0982a74a84fd39dc
Riku Walve <riku.walve@gmail.com>
2015-10-20 14:53:06 +0300

    fix bug with broken user id in session causing infinite redirect loop

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -2,7 +2,6 @@ class FrontpageController < ApplicationController
   def index
     @login_url = ENV['RAILS_ENV'] == 'production' ? '/auth/shibboleth' : users_path
 
-    # redirect_to feed_index_path unless current_user.nil?
-    redirect_to feed_index_path unless session[:user_id].nil?
+    redirect_to feed_index_path unless current_user.nil?
   end
 end
