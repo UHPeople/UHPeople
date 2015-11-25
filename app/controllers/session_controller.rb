0d4d994edc432d1b79d9590dfec8d3faa20fbe8a
Riku Walve <riku.walve@gmail.com>
2015-11-25 14:19:31 +0200

    tweak session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,20 +3,14 @@ class SessionController < ApplicationController
 
   def login
     @user = User.find(params[:id])
-
     session[:user_id] = @user.id
-
-    if current_user.user_hashtags.count < 3
-      redirect_to threehash_path, alert: 'Please add at least three intrests!'
-    else
-      redirect_to feed_index_path
-    end
+    redirect_to feed_index_path
   rescue
     redirect_to feed_index_path
   end
 
   def logout
     session[:user_id] = nil
-    redirect_to ((ENV['RAILS_ENV'] == 'production') ? '/Shibboleth.sso/Logout' : root_path)
+    redirect_to((ENV['RAILS_ENV'] == 'production') ? '/Shibboleth.sso/Logout' : root_path)
   end
 end
