332176d73195f75b43189a337b24117c2d5a4ce1
Riku Walve <riku.walve@gmail.com>
2015-12-15 11:40:21 +0200

    change theme color with js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -131,7 +131,31 @@ var toggleSearch = function(event) {
 	}
 }
 
+$.cssHooks.backgroundColor = {
+	get: function(elem) {
+		if (elem.currentStyle)
+			var bg = elem.currentStyle["backgroundColor"];
+		else if (window.getComputedStyle)
+			var bg = document.defaultView.getComputedStyle(elem,
+					null).getPropertyValue("background-color");
+		if (bg.search("rgb") == -1)
+			return bg;
+		else {
+			bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+			function hex(x) {
+					return ("0" + parseInt(x).toString(16)).slice(-2);
+			}
+			return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+		}
+	}
+}
+
 var ready = function() {
+	if ($(location).attr('pathname') == "/hashtag") {
+		var color = $('.hashtag_bg').css('background-color');
+		$('meta[name="theme-color"]').attr('content', color);
+	}
+
 	$('.search-toggle').on('click', toggleSearch);
 
 	$(function() {
